60f537e35b0ef73b753692ca56fda47787a0b48d
Benoit Souillat <dev.souillat@gmail.com>
2024-10-11 14:39:48 +0200

    Prepare SQL Request on Book Repository for the app

--- repository/BookRepository.php
+++ repository/BookRepository.php
@@ -11,6 +11,26 @@ class BookRepository extends AbstractRepository
         $stmt->execute();
         return $stmt->fetchAll();
     }
+    public function getAllBooksOfUser(int $userID): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM books B WHERE owner = :id");
+        $stmt->bindValue(':id', $userID);
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+    public function getAllBooksAvailables(): array
+    {
+        $stmt = $this->db->prepare("SELECT * FROM books WHERE available = 1");
+        $stmt->execute();
+        return $stmt->fetchAll();
+    }
+    public function getBookById(int $id): stdClass
+    {
+        $stmt = $this->db->prepare("SELECT * FROM books WHERE id = :id");
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
     public function getLastFourthbooks(): array
     {
         $stmt = $this->db->prepare("SELECT * FROM books B INNER JOIN users U ON B.owner = U.id ORDER BY B.id DESC LIMIT 4 ");
